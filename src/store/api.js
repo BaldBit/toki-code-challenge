d3fa51e9f67c07fa74d6855880980fe55056a2e5
ryxle <ryxle@outlook.com>
2019-11-03 01:41:57 +0530

    undoing mistakenly removed sync

--- src/store/api.js
+++ src/store/api.js
@@ -3,8 +3,8 @@ import FlightHelpers from '../utils/flightHelpers';
 import { CHEAP_FLIGHTS, BUSINESS_FLIGHTS } from '../utils/constants';
 
 export default class Api {
-  static fetchCheapFlights() {
-    return axios.get('https://tokigames-challenge.herokuapp.com/api/flights/cheap').then(response => {
+  static async fetchCheapFlights() {
+    return await axios.get('https://tokigames-challenge.herokuapp.com/api/flights/cheap').then(response => {
       if (response.status === 200) {
         return response.data;
       }
@@ -14,8 +14,8 @@ export default class Api {
     });
   }
 
-  static fetchBusinessFlights() {
-    return axios.get('https://tokigames-challenge.herokuapp.com/api/flights/business').then(response => {
+  static async fetchBusinessFlights() {
+    return await axios.get('https://tokigames-challenge.herokuapp.com/api/flights/business').then(response => {
       if (response.status === 200) {
         return response.data;
       }
@@ -25,8 +25,8 @@ export default class Api {
     });
   }
 
-  static getAllFlights() {
-    return axios.all([Api.fetchCheapFlights(), Api.fetchBusinessFlights()])
+  static async getAllFlights() {
+    return await axios.all([Api.fetchCheapFlights(), Api.fetchBusinessFlights()])
     .then(axios.spread(function (cheap, business) {
       // format data according to our own
       const cheapFlights = cheap.data.map(data => FlightHelpers.formatFlightDetails(CHEAP_FLIGHTS, data));
